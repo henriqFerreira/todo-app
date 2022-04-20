ea06425ae8c74f99302d8237f9fcf3f57f8baae5
Henrique Neto <henriqueneto1912@gmail.com>
2022-04-20 19:20:48 -0300

    Adicionado botão para pop-up de nova tarefa

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, StatusBar } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, StatusBar, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 {/* Import dos componentes */}
 import Task from "../components/Tasks"
@@ -13,8 +14,14 @@ const HomeScreen = ({ navigation }) => {
             <ScrollView>
                 <View style={ styles.container }>
                     <View>
-                        {/* Título da seção */}
-                        <Text style={ styles.title }>Tarefas de hoje</Text>
+                        <View style={ styles.header }>
+                            {/* Título da seção */}
+                            <Text style={ styles.title }>Tarefas de hoje</Text>
+                            {/* Botão que abre o pop-up de nova tarefa */}
+                            <TouchableOpacity style={ styles.addTaskButton }>
+                                <Icon style={ styles.addTaskText } name="plus" size={14} color="#000"/>
+                            </TouchableOpacity>
+                        </View>
                         {/* Container que terá todas as tasks, (hoje) */}
                         <View style={ styles.tasksContainer }>
                             {/* Cada task será inserida aqui */}
@@ -33,10 +40,25 @@ const styles = StyleSheet.create({
         padding: 25,
         backgroundColor: '#EDF2FB'
     },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
         color: '#000000'
+    },
+    addTaskButton: {
+        width: 30,
+        height: 30,
+        borderRadius: 20,
+        borderWidth: 2,
+        borderColor: '#ABC4FF',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     tasksContainer: {
         padding: 15
